96f01710048d359b1717c4805258670468d237a2
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-13 17:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const form = document.getElementById("myForm");
+form.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	const firstName = form.elements["First Name"].value;
+	const lastName = form.elements["Last Name"].value;
+	const phone = form.elements["Phone Number"].value;
+    const email = form.elements["Email ID"].value;
+	alert(
+        "First Name: " + firstName + " " +
+        "Last Name: " + lastName + " " +
+        "Phone Number: " + phone + " " +
+        "Email ID: " + email
+    );
+})
